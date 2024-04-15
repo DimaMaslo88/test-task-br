009c604d7575a9d514a7b652dbcbc766288c32a8
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-04-15 19:37:08 +0300

    'first commit'

---
+++ src/ui/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from 'styles/Header.module.css'
+
+export const Header = () => {
+    return (
+        <div className={style.headerContainer}>
+            <h1>Тестовое задание</h1>
+        </div>
+    );
+};
+
