cdec9af91bd14d89a48d6e08b7bb26a09201cfb9
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-04-21 18:11:16 +0300

    'feat/table component'

--- src/ui/components/tableComponent/TableComponent.tsx
+++ src/ui/components/tableComponent/TableComponent.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import style from 'styles/TableContainer.module.css'
+import style from "styles/TableContainer.module.css";
 import LinearIndeterminateProgress from "ui/utils/progress/Progress";
 import { useSelector } from "react-redux";
 import { selectIsLoading, selectTableParams } from "bll/selectors/Selectors";
-import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
+import FilterListOutlinedIcon from "@mui/icons-material/FilterListOutlined";
 
 export const TableComponent = () => {
   const isLoading = useSelector(selectIsLoading);
-  const tableData = useSelector(selectTableParams)
-  if(isLoading){
-    return <LinearIndeterminateProgress/>
+  const tableData = useSelector(selectTableParams);
+  const [isSorting,setIsSorting] = useState<boolean>(false)
+  const sortHandler = ()=>{
+    setIsSorting(!isSorting)
+  }
+  if (isLoading) {
+    return <LinearIndeterminateProgress />;
   }
   return (
-    <TableContainer component={Paper}  className={style.tableContainer}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table" >
+    <TableContainer component={Paper} className={style.tableContainer}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow className={style.tableHeader}>
-            <TableCell>За <br/>период</TableCell> <FilterListOutlinedIcon/>
-            <TableCell align="right">Год</TableCell> <FilterListOutlinedIcon/>
-            <TableCell align="right">Организация</TableCell> <FilterListOutlinedIcon/>
+            <TableCell >За <br />период <FilterListOutlinedIcon className={style.filterIcon} onClick={sortHandler} /></TableCell>
+            <TableCell align="right">Год <FilterListOutlinedIcon className={style.filterIcon} /></TableCell>
+            <TableCell align="right">Организация <FilterListOutlinedIcon className={style.filterIcon} /></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -29,8 +33,8 @@ export const TableComponent = () => {
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {`${new Date(data.rep_beg_period).toLocaleString('default', { month: 'long' })} 
-                - ${new Date(data.rep_end_period).toLocaleString('default', { month: 'long' })}`}
+                {`${new Date(data.rep_beg_period).toLocaleString("default", { month: "long" })} 
+                - ${new Date(data.rep_end_period).toLocaleString("default", { month: "long" })}`}
               </TableCell>
               <TableCell align="right">{`${new Date(data.insert_date).getFullYear()}`}</TableCell>
               <TableCell align="right">Department of Presidential Affairs of the Republic of Belarus</TableCell>
